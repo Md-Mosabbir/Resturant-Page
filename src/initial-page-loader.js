0ead460f2f86b5576898160f58d98ebff5367f0c
Md-Mosabbir <kmosabbir@gmail.com>
2023-02-20 14:53:17 +0600

    Main content finished

--- src/initial-page-loader.js
+++ src/initial-page-loader.js
@@ -40,4 +40,18 @@ export function initialPageLoader () {
   navigationContainer.appendChild(navigationLogo)
   navigationContainer.appendChild(navigationLinks)
   contentID.appendChild(navigationContainer)
+  // ----------------------------------------------------
+
+  const mainContent = document.createElement('main')
+  mainContent.classList.add('main-text')
+  const mainContentDiv = document.createElement('div')
+  const headerTitle = document.createElement('h1')
+  headerTitle.textContent = 'Title'
+  const headerSubtitle = document.createElement('h3')
+  headerSubtitle.textContent = 'Subtitle'
+
+  mainContentDiv.appendChild(headerTitle)
+  mainContentDiv.appendChild(headerSubtitle)
+  mainContent.appendChild(mainContentDiv)
+  contentID.appendChild(mainContent)
 }
