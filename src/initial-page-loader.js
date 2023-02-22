e40a7656e7aec4e7b2fb836413abfde3311b7b5f
Md-Mosabbir <kmosabbir@gmail.com>
2023-02-22 20:40:46 +0600

    Homepage styled

--- src/initial-page-loader.js
+++ src/initial-page-loader.js
@@ -1,3 +1,4 @@
+import './initial-page-loader.css'
 export function initialPageLoader () {
   const contentID = document.querySelector('#content')
 
@@ -7,12 +8,16 @@ export function initialPageLoader () {
   mainContent.classList.add('main-text')
   const mainContentDiv = document.createElement('div')
   const headerTitle = document.createElement('h1')
-  headerTitle.textContent = 'Title'
+  headerTitle.textContent = 'The Bengali Hub'
   const headerSubtitle = document.createElement('h3')
-  headerSubtitle.textContent = 'Subtitle'
+  headerSubtitle.textContent = 'A Vhalo Experience'
+
+  const exploreButton = document.createElement('button')
+  exploreButton.textContent = 'Explore'
 
   mainContentDiv.appendChild(headerTitle)
   mainContentDiv.appendChild(headerSubtitle)
+  mainContentDiv.appendChild(exploreButton)
   mainContent.appendChild(mainContentDiv)
   contentID.appendChild(mainContent)
 }
