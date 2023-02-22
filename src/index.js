412c5f0a3568ab16eefe2b08ae01e0cc1a7ee667
Md-Mosabbir <kmosabbir@gmail.com>
2023-02-23 00:37:11 +0600

    .

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import { initialPageLoader } from './initial-page-loader'
 import { contact } from './contact'
 import { menuPage } from './menu'
 import './main.css'
+// pictures
+import './assets/DALL·E 2023-02-17 14.52.47 - An image of a bengali biriyani with a dark background for the homepage of a resturent website. .png'
+import './assets/DALL·E 2023-02-17 14.53.24 - An image of a bengali biriyani with a dark background for the homepage of a resturent website. .png'
+import './assets/DALL·E 2023-02-17 14.54.00 - An image of a bengali biriyani with a dark background for the homepage of a resturent website 4K.  .png'
+import './assets/DALL·E 2023-02-17 14.57.43 - different styles of biriyani with bengali essence for resturant menu page presentation that will attract anyone.png'
+import './assets/restaurant-location.png'
 
 initialPageLoader()
 // eslint-disable-next-line no-unused-vars
