7e00f2083df1f4ab3286cdc6811d4efffe847a29
Md-Mosabbir <kmosabbir@gmail.com>
2023-02-23 01:04:48 +0600

    image issue prolly solved by file loader

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,6 @@
-
+import './assets/DALL·E 2023-02-17 14.52.47 - An image of a bengali biriyani with a dark background for the homepage of a resturent website. .png'
+import './assets/DALL·E 2023-02-17 14.53.24 - An image of a bengali biriyani with a dark background for the homepage of a resturent website. .png'
+import './assets/DALL·E 2023-02-17 14.57.43 - different styles of biriyani with bengali essence for resturant menu page presentation that will attract anyone.png'
 export function menuPage () {
   const contentID = document.querySelector('#content')
 
